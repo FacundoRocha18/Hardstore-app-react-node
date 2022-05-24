74ac8430e7c1b773f1de1d41182f1343e7ff3f5b
Facundo Rocha <facundo.rocha@outlook.com>
2022-05-24 12:43:25 -0300

    updated 24/5

--- src/helpers/getCategories.js
+++ src/helpers/getCategories.js
@@ -1,7 +1,7 @@
 
 const getCategories = async() => {
 
-    const url = `http://localhost:8000/`;
+    const url = `http://192.168.1.8:8000/`;
     const response = await fetch( url );
     const { data } = await response.json();
 
