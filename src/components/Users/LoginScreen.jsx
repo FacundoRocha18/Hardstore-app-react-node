2af2a3d9d3775536f3af86f5c1f64c1cff33771a
Facundo Rocha <facundo.rocha@outlook.com>
2022-06-21 14:46:29 -0300

    changed: css documents reference

--- src/components/Users/LoginScreen.jsx
+++ src/components/Users/LoginScreen.jsx
@@ -7,7 +7,7 @@ import { Link, useNavigate } from 'react-router-dom'
 import LoadingScreen from '../LoadingScreen';
 
 /* Styles imports -------------------------------- */
-import style from "./LoginScreen.module.css";
+import style from "./LoginScreen.css";
 import css from "classnames";
 
 
