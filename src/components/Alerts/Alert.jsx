766133073c5f63555992955178015a2f9281f926
Facundo Rocha <facundo.rocha@outlook.com>
2022-06-21 12:02:12 -0300

    Changed CSS links

--- src/components/Alerts/Alert.jsx
+++ src/components/Alerts/Alert.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect } from 'react'
 
 /* Styles imports */
-import style from "../Alerts/Alert.module.css";
+import style from "./Alert.module.css";
 import css from "classnames";
 
 
--- src/components/Products/ProductsCard.jsx
+++ src/components/Products/ProductsCard.jsx
@@ -7,7 +7,7 @@ import { Cloudinary } from "@cloudinary/url-gen";
 import { fill, thumbnail } from "@cloudinary/url-gen/actions/resize";
 
 /* Styles imports -------------------------------- */
-import style from "../Products/ProductsCard.module.css";
+import style from "./ProductsCard.module.css";
 import css from "classnames";
 
 
